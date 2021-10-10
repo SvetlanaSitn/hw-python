d3fdd9d60c609738fd69d3f80552092018a9a7d3
SvetlanaSitn <46673852+SvetlanaSitn@users.noreply.github.com>
2021-10-10 11:46:30 +0300

    new hw 2

---
+++ ls4.py
@@ -0,0 +1,30 @@
+##while True: 
+##    s1=input("Введите число 1: ")
+##    s2=input("Введите число 2: ")
+##    if  s1.isdigit() == True and s2.isdigit() == True:
+##        s3 = int(s1)+ int(s2)
+##        print(int(s3))
+
+##s1 = ''
+##while s1.isdigit() == False:
+##    s1 = input("Введите число 1: ")
+##
+##s2 = ''
+##while s2.isdigit() == False:
+##    s2 = input("Введите число 2: ")
+##
+##s3 = int(s1)+ int(s2)
+##print(int(s3))
+
+##while True:
+##    num1 = input("Введите число 1: ")
+##    num2 = input("Введите число 2: ")
+##    if num1.isdigit() and num2.isdigit():
+##        num3 = int(num1) + int(num2)
+##        print("Сумма чисел:", num3)
+##    else:
+##        print("Вы ввели не число")
+
+s2 = "Hello World!"
+s2 = '!' + s2[1:-1]
+print(s2)
